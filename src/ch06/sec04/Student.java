5facbc6ea301d9afc61553603b9f2d6e0f63847a
민정 <kmj20050227@gmail.com>
2025-11-11 22:57:52 +0900

    9주차 실습 과제

---
+++ src/ch06/sec04/Student.java
@@ -0,0 +1,15 @@
+package ch06.sec04;
+
+public class Student {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		System.out.println("s1 변수가 Student객체를 점프합니다.");
+		
+		Student s2 = new Student();
+		System.out.println("s2 변수가 Student객체를 참조합니다.");
+
+	}
+
+}
